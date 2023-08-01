62213f227dae26a1eb0a38a8ecc07711a67ec9c7
Egor Dezhic <edezhic@gmail.com>
2023-08-01 19:30:25 +0300

    lil thing

--- pwrs-app/lib.rs
+++ pwrs-app/lib.rs
@@ -5,6 +5,12 @@ fn ui_service() -> Router {
     Router::new().route("/", render!(homepage))
 }
 
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen::prelude::wasm_bindgen]
+pub async fn serve(host: &str, event: pwrs_sw::FetchEvent) {
+    pwrs_sw::process_fetch_event(ui_service, host, event).await
+}
+
 #[cfg(not(target_arch = "wasm32"))]
 #[derive(rust_embed::RustEmbed, Clone, Copy)]
 #[folder = "./pub"]
@@ -16,10 +22,4 @@ pub fn service() -> Router {
         .merge(ui_service())
         .layer(pwrs_host::embed(Assets))
         .layer(pwrs_host::http_tracing())
-}
-
-#[cfg(target_arch = "wasm32")]
-#[wasm_bindgen::prelude::wasm_bindgen]
-pub async fn serve(host: &str, event: pwrs_sw::FetchEvent) {
-    pwrs_sw::process_fetch_event(ui_service, host, event).await
 }
