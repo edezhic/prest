d99b17d24c90f8322afcc6d8dfcbfa6b0f520f3a
Egor Dezhic <edezhic@gmail.com>
2023-08-01 19:38:14 +0300

    simplify a bit

--- pwrs-app/lib.rs
+++ pwrs-app/lib.rs
@@ -1,5 +1,29 @@
-mod homepage;
 use pwrs::*;
+
+mod homepage {
+    pub fn render() -> maud::Markup {
+        maud::html!(
+            html {
+                head {
+                    title {"PWRS app"}
+                    link rel="icon" href="/favicon.ico" {}
+                    link rel="manifest" href="/.webmanifest" {}
+                    link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css" {}
+                    @if cfg!(any(target_arch = "wasm32", feature = "sw")) { script src="/include_sw.js" {} }
+                    meta name="viewport" content="width=device-width, initial-scale=1.0";
+                }
+                body {
+                    h1{"PWRS"}
+                    h2{"powers for you app!"}
+                    p{b{"Progressive"}" application that starts as a webpage and can bootstrap up to native"}
+                    p{"based on " b{"Web"}" standards such as HTML, CSS, TS/JS, Web APIs, WASM et cetera"}
+                    p{"written in " b{"RuSt"}" for safety and speed on a wide range of platforms"}
+                    
+                }
+            }
+        )
+    }       
+}
 
 fn ui_service() -> Router {
     Router::new().route("/", render!(homepage))
