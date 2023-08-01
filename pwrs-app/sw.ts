da4c771ccae939fb5f19ec89af39dec407e9ff1d
Egor Dezhic <edezhic@gmail.com>
2023-08-01 19:47:47 +0300

    skip waiting sw activation

--- pwrs-app/sw.ts
+++ pwrs-app/sw.ts
@@ -1,12 +1,12 @@
 // TypeScript shenanigans 
 /// <reference lib="WebWorker" />
-const { addEventListener, clients, location } = self as unknown as ServiceWorkerGlobalScope;
+const { addEventListener, clients, skipWaiting, location } = self as unknown as ServiceWorkerGlobalScope;
 
 // import WASM module initializer and generated bindings 
 // lib.js and lib.d.ts will be generated and updated in this folder right before bundling
 import init, { serve } from "./lib.js";
 // holding SW installation completion until wasm module is initialized
-addEventListener('install', event => event.waitUntil(init('/sw.wasm')));
+addEventListener('install', event => event.waitUntil(Promise.all([init('/sw.wasm'), skipWaiting()])));
 // force all tabs/windows to use SW for requests
 addEventListener("activate", event => event.waitUntil(clients.claim()));
 // using rust service to handle fetch event
