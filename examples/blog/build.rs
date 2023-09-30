67887cfa70d7e5495dcc5093904c8df8d8bcdc6f
edezhic <4923054+edezhic@users.noreply.github.com>
2023-09-30 16:44:03 +0000

    disable blog pwa while figuring out replit

--- examples/blog/build.rs
+++ examples/blog/build.rs
@@ -1,6 +1,8 @@
 fn main() {
-    #[cfg(all(feature = "host", not(debug_assertions)))] 
-    prest::build::generate_pwa_assets();
+    // DISABLED WHILE SETTING UP WASM TOOLCHAIN ON REPLIT
+    // https://ask.replit.com/t/multiple-rust-toolchains-with-nix/69334 
+    //#[cfg(all(feature = "host", not(debug_assertions)))] 
+    //prest::build::generate_pwa_assets();
 
     #[cfg(feature = "host")] 
     prest::build::bundle_scss("./styles.scss", "ui.css");
