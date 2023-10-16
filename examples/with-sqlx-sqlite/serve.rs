11b792841f2e627be3dc46a34360d4cd57202d9e
edezhic <4923054+edezhic@users.noreply.github.com>
2023-10-16 10:57:31 +0000

    todo ui wip

--- examples/with-sqlx-sqlite/serve.rs
+++ examples/with-sqlx-sqlite/serve.rs
@@ -1,6 +1,6 @@
 use prest::*;
 use sqlx::{migrate, query, query_as, Pool, Sqlite};
-use todo::{Todo, page};
+use todo::Todo;
 
 static DB: Lazy<Pool<Sqlite>> = Lazy::new(|| {
     sqlx::sqlite::SqlitePoolOptions::new()
@@ -61,11 +61,27 @@ async fn todos() -> Markup {
     html!(@for todo in todos {(todo)})
 }
 
+pub fn page(content: Markup) -> Markup {
+    html!{ html data-theme="dark" {
+        (Head::default().title("Todo"))
+        body."container" hx-target="article" {
+            form hx-put="/" {
+                label for="task" {"Task description:"}
+                input type="text" name="task" {}
+                input type="submit" value="Create" {}
+            }
+        }
+        article {(content)}
+    }}
+}
+
 mod todo {
-    // dep:sqlx without runtime and other default-features
+    use prest::*;
     use sqlx::FromRow;
     use serde::Deserialize;
-    #[derive(Debug, FromRow, Deserialize)]
+    use uuid::Uuid;
+    
+    #[derive(FromRow, Deserialize)]
     pub struct Todo {
         #[serde(default)]
         pub uuid: String,
@@ -76,27 +92,26 @@ mod todo {
     }
     impl Todo {
         pub fn new_uuid() -> String {
-            
+            uuid::Uuid::new_v4().to_string()
         }
     }
     impl Render for Todo {
         fn render(&self) -> Markup {
-            let id = uuid::Uuid::parse_str(&self.uuid).unwrap();
-            let id = format!("uuid:{id}");
-            let done = if self.done {
-                format!("set @checked then set @disabled")
-            } else {
-                format!("on change trigger submit on #{id}")
-            };
+            let id = Uuid::parse_str(&self.uuid).unwrap();
+            let id = format!("uuid-{id}");
+            let cb = format!("on change from trigger submit on #{id}");
             html!(
                 div style="height: 64px; display: flex; justify-content: space-between; align-items: center;" {
-                    form hx-patch="/" id={(id)} style="margin-bottom: 0px;" {
+                    form #(id) hx-patch="/"  style="margin-bottom: 0px;" {
                         input type="hidden" name="uuid" value={(self.uuid)} {}
                         input type="hidden" name="done" value={(self.done)} {}
-                        label {
-                            input type="checkbox" _={(done)} {}
+                        label { 
+                            @match self.done {
+                                false => { input .(id) type="checkbox" _={(cb)} {} },
+                                true  => { input .(id) type="checkbox" _={(cb)} checked {} },
+                            }
                             {(self.task)}
-                        }
+                        }    
                     }
                     form hx-delete="/" style="margin-bottom: 0px;" {
                         input type="hidden" name="uuid" value={(self.uuid)} {}
@@ -105,18 +120,5 @@ mod todo {
                 }
             )
         }
-    }
-    
-    pub fn page(content: Markup) -> Markup {
-        html!( html data-theme="dark" {
-            (Head::default().title("Todo"))
-            body."container" hx-target="body" {
-                form hx-put="/" {
-                    label for="task" {"Task description:"}
-                    input type="text" name="task" {}
-                    input type="submit" value="Create" {}
-            }
-            article {(content)}
-        })
     }
 }
