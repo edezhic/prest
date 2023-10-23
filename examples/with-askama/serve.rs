815a4eb2bd120f63f42ab0895d11defd78d5343c
edezhic <4923054+edezhic@users.noreply.github.com>
2023-10-23 19:14:46 +0000

    init askama example

---
+++ examples/with-askama/serve.rs
@@ -0,0 +1,13 @@
+use prest::*;
+
+#[derive(askama::Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate<'a> {
+    name: &'a str,
+}
+
+#[tokio::main]
+async fn main() {
+    let service = Router::new().route("/", get(Html(HelloTemplate { name: "world" }.render().unwrap())));
+    serve(service, Default::default()).await
+}
