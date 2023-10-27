a41737ea40715fd885d16d8d4bf8e986e20164b2
Egor Dezhic <edezhic@gmail.com>
2023-10-27 15:28:27 +0300

    lil upd

--- examples/blog/posts/roadmap.md
+++ examples/blog/posts/roadmap.md
@@ -1,9 +1,16 @@
 This is a hobby project, there are no solid long-term plans and api is unstable. However, [the overall idea](https://prest.blog/motivation) seems quite promising to me so there are things I'll likely build next:
 
 - update default logo+favicon and how they are used with PWAOptions
-- logs for the SW: simple `console_log`s under cfg(debug_assrts) or maybe tracing ([1](https://github.com/old-storyai/tracing-wasm), [2](https://docs.rs/tracing-chrome/latest/tracing_chrome/))
-- dev hyperscript/wasm ui tools
-- GlueSQL on the client - [doc](https://gluesql.org/docs/0.14/getting-started/javascript-web)
+- TypeScript, SASS, https and other simple examples => docs
+- Embed += paths of the embedded files
+- + readmes for examples and include them into the blog
 
 ### Publish
 It's not on [crates.io](https://crates.io/crates/prest) yet because it depends on the latest unpublished [axum](https://github.com/tokio-rs/axum) and related packages. Awaiting them to upload the first alpha version.
+
+
+### Future considerations
+
+- logs for the SW: simple `console_log`s under cfg(debug_assrts) or maybe tracing ([1](https://github.com/old-storyai/tracing-wasm), [2](https://docs.rs/tracing-chrome/latest/tracing_chrome/))
+- GlueSQL on the client - [doc](https://gluesql.org/docs/0.14/getting-started/javascript-web)
+- dev hyperscript/wasm ui tools
--- utils/sw.rs
+++ utils/sw.rs
@@ -3,7 +3,7 @@ use crate::*;
 extern crate console_error_panic_hook;
 pub use console_error_panic_hook::set_once as set_panic_hook;
 use js_sys::{Array, Reflect, Set, Uint8Array, Promise};
-pub use web_sys::{FetchEvent, ServiceWorkerGlobalScope, console::log_1 as console_log};
+pub use web_sys::{FetchEvent, ServiceWorkerGlobalScope, console};
 use wasm_bindgen::{JsCast, JsValue};
 
 pub use wasm_bindgen::prelude::wasm_bindgen;
@@ -16,18 +16,29 @@ pub async fn serve(mut router: Router, sw: ServiceWorkerGlobalScope, event: Fetc
     if request.uri().host() != Some(host) {
         return;
     }
+
     // pass the request to the router
-    let Ok(response) = router.call(request).await else { return };
-    // proceed only if OK
-    if response.status().as_u16() != 200 {
-        return;
+    let response = match router.call(request).await {
+        Ok(res) => res,
+        Err(e) => {
+            #[cfg(debug_assertions)]
+            console::log_3(&"SW error while processing ".into(), &event.request(), &format!(" : {:?}", e).into());
+            return 
+        }
+    };
+    
+    if response.status().as_u16() >= 400 {    
+        return
     }
-    let promise = axum_response_into_promise(response);
+    
+    let response = axum_response_to_websys(response);
+    let promise = wasm_bindgen_futures::future_to_promise(response);
     event.respond_with(&promise).unwrap();
 }
 
 pub async fn fetch_into_axum_request(fetch_event: &web_sys::FetchEvent) -> http::Request<Body> {
     let fetch_request = fetch_event.request();
+
     // initialize a new request builder with method and url
     let mut request = http::request::Builder::new()
         .method(fetch_request.method().as_str())
@@ -78,12 +89,7 @@ pub async fn fetch_into_axum_request(fetch_event: &web_sys::FetchEvent) -> http:
     request.body(Body::from(buf)).unwrap()
 }
 
-pub fn axum_response_into_promise(response: http::Response<Body>) -> Promise {
-    let websys_response = axum_response_to_websys(response);
-    wasm_bindgen_futures::future_to_promise(websys_response)
-}
-
-async fn axum_response_to_websys(response: http::Response<Body>) -> Result<JsValue, JsValue> {
+pub async fn axum_response_to_websys(response: http::Response<Body>) -> Result<JsValue, JsValue> {
     // init web_sys::Headers
     let websys_response_headers = web_sys::Headers::new()?;
     // collect http::HeaderMap into web_sys::Headers
@@ -109,6 +115,7 @@ async fn axum_response_to_websys(response: http::Response<Body>) -> Result<JsVal
 
     // initialize web_sys::Response with body and parts
     let websys_response = web_sys::Response::new_with_opt_u8_array_and_init(body, &parts)?;
+
     // convert into JsValue
     Ok(websys_response.into())
 }
