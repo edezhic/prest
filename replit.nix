cd8547da7767481a435d502445c0eb08537ca94d
egor dezhic <edezhic@gmail.com>
2023-12-11 15:38:11 +0300

    clean up replit configs

--- replit.nix
+++ replit.nix
@@ -1,12 +1,7 @@
 { pkgs }: {
 	deps = [
-    pkgs.rustc
+		pkgs.rustc
 		pkgs.rustfmt
 		pkgs.cargo
-		# disabled while not running here to speed up work with docs
-		#pkgs.rust-analyzer
-		#pkgs.pkg-config
-		#pkgs.openssl # for candle-mistral (hf-hub model download)
-		#pkgs.sqlite # for sqlx-sqlite example
 	];
 }
